cfae8bad9c30a16a8b34fae1aa9a7ed2b75ec649
sadnesszephyr <tima@nopublic.ru>
2022-11-02 04:52:29 +0400

    Updated config

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,20 @@
 export const staffIds = [
 	'521048525102907408', // sadnesszephyr
 	'689472427150803014', // adrenaline
-	'925870482627117136' // yaku
+	'973711182647156737'  // yaku
 ]
 
 export const staffRolesIds = [
 	'936788938067173376', // @Dev
-	'979874004020105266' // @Staff
+	'979874004020105266'  // @Staff
 ]
+
+export const monoLoungeInviteLink = 'https://discord.gg/exWW6gGcyw'
 
 export const monoLoungeGuildId = '913870886254288977'
 
 export const devGuildsIds = [
 	monoLoungeGuildId,
 	'832962403058188319', // Mono Dev
-	'838128442976370749' // Mono Emojis
+	'838128442976370749'  // Mono Emojis
 ]
-
-export const monoLoungeInviteLink = 'https://discord.gg/exWW6gGcyw'
